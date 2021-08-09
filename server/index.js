f73d6dd05f6ae60e174bdfddc060a54385bc1337
cececlar <cclaridge@brainstation.io>
2021-08-09 14:56:14 -0400

    remove conditional logic from dotenv import

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const userRoutes = require("./routes/users");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/users", userRoutes);
 
