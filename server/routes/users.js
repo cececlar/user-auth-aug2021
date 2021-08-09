16f15535becd49bb3d6c5a58d5247cb99ddb2eed
cececlar <cclaridge@brainstation.io>
2021-08-09 08:58:33 -0400

    add TODO notes throughout server

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,7 +6,7 @@ const Task = require("../models/task");
 const authorize = require("../middleware/authorize");
 
 //CREATE new user
-
+//TODO: Update this so that when a new user is created, their password is encrypted before it is stored on the database. Then, generate a JSON web token and send it with the response.
 router.post("/", (req, res) => {
   const { password } = req.body;
   new User({ ...req.body, password: password })
@@ -20,6 +20,7 @@ router.post("/", (req, res) => {
 });
 
 //LOGIN user
+//TODO: Verify that the user has entered the password associated with their account. Then, generate a JSON web token and send it with the response.
 router.post("/login", async (req, res) => {
   User.where({ email: req.body.email })
     .fetch()
@@ -32,6 +33,7 @@ router.post("/login", async (req, res) => {
 });
 
 //Get Current User
+//TODO: Write a middleware function in server/middleware/authorize.js to verify that a user has sent their JSON web token in the authorization headers of their request, and that it is valid. Invoke the middleware function.
 router.get("/current", (req, res) => {
   User.where({ id: 1 })
     .fetch()
