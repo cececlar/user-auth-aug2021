490e58cddea8826b2ec93c9cb959e8e78173af54
cececlar <cclaridge@brainstation.io>
2021-08-09 12:19:59 -0400

    update .env.sample variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.scss";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
