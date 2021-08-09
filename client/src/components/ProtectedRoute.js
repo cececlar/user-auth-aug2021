dc3f9de377105ca98ffac1cd0e6caf819c47d0a2
cececlar <cclaridge@brainstation.io>
2021-08-09 14:55:18 -0400

    update ProtectedRoute to console.log reactRouterProps for clarity

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const token = sessionStorage.getItem('token');
+const ProtectedRoute = ({ component: Component, ...reactRouterProps }) => {
+  const token = sessionStorage.getItem("token");
+  console.log(reactRouterProps);
   return (
     <Route
-      {...rest}
+      {...reactRouterProps}
       render={(routeProps) =>
         !token ? <Redirect to="/login" /> : <Component {...routeProps} />
       }
